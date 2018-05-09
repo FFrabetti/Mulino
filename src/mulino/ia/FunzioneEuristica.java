8c198a9e34f28bce1ccaef8cadf69ea5fa3f81b6
NicHub-Semprini <38130317+NicHub-Semprini@users.noreply.github.com>
2018-05-09 18:47:10 +0200

    prima bozza euristica

--- src/mulino/ia/FunzioneEuristica.java
+++ src/mulino/ia/FunzioneEuristica.java
@@ -5,6 +5,7 @@ import java.util.List;
 import aima.search.framework.HeuristicFunction;
 import game.general.GameAction;
 import it.unibo.ai.didattica.mulino.domain.State.Checker;
+import it.unibo.ai.didattica.mulino.domain.State.Phase;
 import mulino.MulinoAction;
 import mulino.MulinoState;
 import mulino.Phase1MulinoAction;
@@ -12,6 +13,17 @@ import mulino.Phase23MulinoAction;
 import mulino.Position;
 
 public class FunzioneEuristica implements HeuristicFunction {
+
+	private int[][] valori = new int[3][3];
+
+	public FunzioneEuristica() {
+		valori[0][0] = 1;
+		valori[1][0] = 7;
+		valori[0][1] = 3;
+		valori[2][0] = 29;
+		valori[0][2] = 15;
+		valori[1][1] = 0;
+	}
 
 	/*
 	 * Il valore di uno stato dipende da due parametri: 1) la migliore mossa che mi
@@ -29,33 +41,38 @@ public class FunzioneEuristica implements HeuristicFunction {
 	public double getHeuristicValue(Object state) {
 		MulinoState s = (MulinoState) state;
 
-		for (GameAction a : s.legitActions()) {
-			MulinoAction mossa = (MulinoAction) a;
-
+		double value = 0;
+		int maxActionVal = 0;
+		if (s.getCurrentPhase() == Phase.FIRST) {
+			for (GameAction a : s.legitActions()) {
+				maxActionVal = phase1ActionValue(s, (Phase1MulinoAction)a);
+				if(maxActionVal == 30)
+					break;
+			}
+			value = maxActionVal; // in fase 1 un buono stato coincide con una buona mossa
+		} else {
+			for (GameAction a : s.legitActions()) {
+				maxActionVal = phase23ActionValue(s, (Phase23MulinoAction)a);
+				if(maxActionVal == 30)
+					break;
+			}
+			value = maxActionVal + stateValue(s); // in fase 23 sullo stato pesano anche il numero di pedine rimaste
 		}
-		return 0;
+		return value;
 	}
 
-	private double mulinoStateValue(MulinoState state) {
+	private int stateValue(MulinoState state) {
 		return (state.getCheckers(state.getDutyPlayer()) - state.getCheckers(state.enemyPlayer()));
 	}
-
-	private double phase1ActionValue(Phase1MulinoAction action) {
-		// no perform
-		return 0;
-	}
-
-	private double phase23ActionValue(Phase23MulinoAction action) {
-		// sì perform
-		return 0;
-	}
-
-	private double trisValue(MulinoState state, Position xy) {
-		double value = 0;
+	
+	private int phase1ActionValue(MulinoState state, Phase1MulinoAction action) {
 		int dp = 0;
 		int ep = 0;
-		for(Position p : state.getRow(xy)) {
-			if(!p.equals(xy)) {
+		int val = 0;
+
+		// controllo la riga
+		for(Position p : state.getRow(action.getTo())) {
+			if(!p.equals(action.getTo())) {
 			Checker c = state.getBoard().getChecker(p);
 			if(c == state.getDutyPlayer())
 				dp++;
@@ -63,8 +80,55 @@ public class FunzioneEuristica implements HeuristicFunction {
 				ep++;
 			}
 		}
-		value = 
-		return 0;
+		val = val + valori[dp][ep];
+		// controllo la colonna
+		dp = 0;
+		ep = 0;
+		for(Position p : state.getColumn(action.getTo())) {
+			if(!p.equals(action.getTo())) {
+			Checker c = state.getBoard().getChecker(p);
+			if(c == state.getDutyPlayer())
+				dp++;
+			else if(c == state.getDutyPlayer())
+				ep++;
+			}
+		}
+		val = val + valori[dp][ep];
+		
+		return val;
+	}
+
+	private int phase23ActionValue(MulinoState state, Phase23MulinoAction action) {
+		int dp = 0;
+		int ep = 0;
+		int val = 0;
+
+		// controllo la riga
+		for(Position p : state.getRow(action.getTo())) {
+			if(!p.equals(action.getTo()) && !p.equals(action.getFrom())) {
+			Checker c = state.getBoard().getChecker(p);
+			if(c == state.getDutyPlayer())
+				dp++;
+			else if(c == state.getDutyPlayer())
+				ep++;
+			}
+		}
+		val = val + valori[dp][ep];
+		// controllo la colonna
+		dp = 0;
+		ep = 0;
+		for(Position p : state.getColumn(action.getTo())) {
+			if(!p.equals(action.getTo()) && !p.equals(action.getFrom())) {
+			Checker c = state.getBoard().getChecker(p);
+			if(c == state.getDutyPlayer())
+				dp++;
+			else if(c == state.getDutyPlayer())
+				ep++;
+			}
+		}
+		val = val + valori[dp][ep];
+		
+		return val;
 	}
 
 }
